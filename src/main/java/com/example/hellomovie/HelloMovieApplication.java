32804430c3e5c280d34d9c8cd823295e5cb160a2
HyunSik Ryu <hsryu123@naver.com>
2023-10-06 22:23:33 +0900

    initial commit

---
+++ src/main/java/com/example/hellomovie/HelloMovieApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellomovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMovieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloMovieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hellomovie/HelloMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellomovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMovieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
